b042216b8f65c9c601663f6c23bc7cc90099ca08
mac <eng.memohamed@gmail.com>
2020-01-27 00:52:56 +0200

    Design of Add Product to store

---
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/AddStoreItem.kt
@@ -0,0 +1,13 @@
+package com.example.a7storenavigationdrawer.ui.addItemToStore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebaseauthwithmvvm.R
+
+class AddStoreItem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_storage_item)
+    }
+}
---
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/StoreItemViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.a7storenavigationdrawer.ui.addItemToStore
+
+class StoreItemViewModel {
+}
