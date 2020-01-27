27a4984cab4842f990374f87e4905bc9294fe58b
Mohamed Medhat <eng.memohamed@gmail.com>
2020-01-27 17:18:59 +0200

    hiding toolbar and redesign add product

--- app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/AddStoreItemActivity.kt
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/AddStoreItemActivity.kt
@@ -1,13 +1,16 @@
-package com.example.a7storenavigationdrawer.ui.addItemToStore
+package com.example.firebaseauthwithmvvm.ui.addItemToStore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.firebaseauthwithmvvm.R
 
-class AddStoreItem : AppCompatActivity() {
+class AddStoreItemActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_add_storage_item)
+
+
     }
 }
