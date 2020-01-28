660ccadaa5aa239a7f87742855cfc631d1984bcf
Mohamed Medhat <eng.memohamed@gmail.com>
2020-01-28 16:05:38 +0200

    observable scroll view

--- app/src/main/java/com/example/firebaseauthwithmvvm/ui/storeProducts/ProductPreviewActivity.kt
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/storeProducts/ProductPreviewActivity.kt
@@ -1,10 +1,17 @@
 package com.example.firebaseauthwithmvvm.ui.storeProducts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import com.example.firebaseauthwithmvvm.R
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.activity_product_preview.*
 
-class test : AppCompatActivity() {
+class ProductPreviewActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
+
+    private val PERCENTAGE_TO_SHOW_IMAGE = 300
+    private var mMaxScrollSize = 0
+    private var mIsImageHidden = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +19,41 @@ class test : AppCompatActivity() {
         //hiding the toolbar
         supportActionBar!!.hide()
 
-        setContentView(R.layout.store_item_row_test)
+        setContentView(R.layout.activity_product_preview)
+
+    }
+
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        appBar.addOnOffsetChangedListener(this)
+
+    }
+
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        if (mMaxScrollSize == 0) mMaxScrollSize = appBarLayout!!.totalScrollRange
+
+        val currentScrollPercentage: Int = (Math.abs(verticalOffset) * 100
+                / mMaxScrollSize)
+
+        if (currentScrollPercentage >= PERCENTAGE_TO_SHOW_IMAGE) {
+            if (!mIsImageHidden) {
+                mIsImageHidden = true
+                ViewCompat.animate(ivWantToCollapse).scaleY(0F).scaleX(0F)
+                    .withEndAction({ ivWantToCollapse.setEnabled(false) }).start()
+            }
+        }
+
+        if (currentScrollPercentage < PERCENTAGE_TO_SHOW_IMAGE) {
+            if (mIsImageHidden) {
+                mIsImageHidden = false
+                ViewCompat.animate(ivWantToCollapse).scaleY(1F).scaleX(1F)
+                    .withEndAction({ ivWantToCollapse.setEnabled(true) }).start()
+            }
+        }
+
+
     }
 }
