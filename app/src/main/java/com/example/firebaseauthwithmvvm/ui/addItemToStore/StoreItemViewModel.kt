a09c95b8f683d517c6abced86a6a31b0e42b0ec3
mac <eng.memohamed@gmail.com>
2020-02-04 02:57:23 +0200

    Add item Config

--- app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/StoreItemViewModel.kt
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/addItemToStore/StoreItemViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.a7storenavigationdrawer.ui.addItemToStore
 
-class StoreItemViewModel {
+import android.app.Activity
+import android.content.Intent
+import android.view.View
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.ViewModel
+
+class StoreItemViewModel : ViewModel() {
+
+
+    fun uploadProductImage() {
+        val intent: Intent = Intent()
+        intent.type = "image/*"
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+//        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
 }
--- app/src/main/java/com/example/firebaseauthwithmvvm/utils/ViewUtils.kt
+++ app/src/main/java/com/example/firebaseauthwithmvvm/utils/ViewUtils.kt
@@ -2,6 +2,7 @@ package com.example.firebaseauthwithmvvm.utils
 
 import android.content.Context
 import android.content.Intent
+import com.example.firebaseauthwithmvvm.ui.addItemToStore.AddStoreItemActivity
 import com.example.firebaseauthwithmvvm.ui.add_product.addProductActivity
 import com.example.firebaseauthwithmvvm.ui.auth.LoginActivity
 import com.example.firebaseauthwithmvvm.ui.home2.Main2Activity
@@ -20,7 +21,7 @@ fun Context.startLoginActivity() =
     }
 
 fun Context.startAddProduct() =
-    Intent(this, addProductActivity::class.java).also {
+    Intent(this, AddStoreItemActivity::class.java).also {
         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(it)
 
